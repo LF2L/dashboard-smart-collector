e5886100f1c39d2bb088462f0871daede37cb2ea
fabbiocrux <fabbiocrux.ms@gmail.com>
2023-02-20 12:06:53 +0100

    icons

---
+++ R/Places.R
@@ -0,0 +1,12 @@
+# Establishing the places
+Places <- 
+   tribble(~Place, ~Lat, ~Long, ~Type,
+           "Green Fablab", 48.69354778372888, 6.199223427009557, "Education", 
+           "ENSGSI", 48.69508092375094, 6.194086052304349, "Education",
+           "ENSGM",  48.695897093034866, 6.193120953312192, "Education",
+           "MJC Bazin", 48.69739157499988, 6.194576095053032, "Association",
+           "SNN", 48.693487765703644, 6.2016121881502375, "Sport club",
+           "Curves", 48.69812957589187, 6.2005961050660385, "Sport club",
+           "VNF", 48.69199572042067, 6.1951544736381745, "Enterprise",
+           "MAIF", 48.693154051864724, 6.196146452736703, "Enterprise"
+   )
